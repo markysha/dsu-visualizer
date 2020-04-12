e78539bc289cb0c8c2b08a17aa4d144dac63b014
Mark Korneychik <krnejchik@mail.ru>
2020-04-13 02:44:23 +0300

    graph constructor

--- src/classes/graph.js
+++ src/classes/graph.js
@@ -1,6 +1,17 @@
 export default class Graph {
-
   constructor(graph) {
-    graph.split("\n");
+    this.edges = [];
+    this.vertices = {};
+
+    graph
+    .split("\n")
+    .forEach(line => {
+      if (!line) return;
+      let numbers = line.split(" ").map(word => +word);
+      if (numbers.length !== 3) return;
+      let [from, to, weight] = numbers;
+      this.edges.push({ from, to, weight });
+      this.vertices = Object.assign(this.vertices, { to, from });
+    });
   }
 }
--- src/reducers/graphReducer.js
+++ src/reducers/graphReducer.js
@@ -3,7 +3,6 @@ import Graph from '../classes/graph';
 export default (state="", action) => {
   switch (action.type) {
     case "UPDATE_GRAPH":
-      let x = new Graph(action.payload);
       return action.payload;
       break;
     default:
